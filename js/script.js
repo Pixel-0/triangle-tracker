fb99c260bf27470ef4a33eec9ce99b9dd6f0c002
Pixel-0 <tiny0pixel@gmail.com>
2019-06-01 16:22:41 +0300

    edit js file.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
-function result() {
+ function tracker() {
 
   var hypotenuse = document.getElementById('hypotenuse').value;
   var adjascent = document.getElementById('adjascent').value;
   var opposite = document.getElementById('opposite').value;
+  var answer=document.getElementById('answer');
 
   if (hypotenuse === opposite && opposite === hypotenuse && hypotenuse === adjascent) {
-    alert("Equilateral");
+    answer.textContent='Equilateral: All sides are equal!';
   } else if (hypotenuse === opposite || hypotenuse === adjascent || opposite === adjascent) {
-    alert("Isosceles");
+    answer.textContent="It's an isoscelece ";
   } else if ((hypotenuse + opposite) <= adjascent || (hypotenuse + adjascent) <= opposite || (opposite + adjascent) <= hypotenuse) {
-    alert("Not a definite triangle");
+    answer.textContent="It's not a definite triangle ";
   } else {
-    alert("Scalene");
+    answer.textContent="It's a scalene triangle";
   }
 
+}
+
+function reset(){
+  var hypotenuse=document.getElementById('hypotenuse').value;
+  var adjascent=document.getElementById('adjascent').value;
+  var opposite=document.getElementById('opposite').value;
+  var answer=document.getElementById('answer');
 }
