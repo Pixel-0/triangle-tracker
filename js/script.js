ccb2771d2c6e76788e9b31b3f899deaf0200cd14
Pixel-0 <tiny0pixel@gmail.com>
2019-06-03 03:39:43 +0300

    ident correctly

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
  function tracker() {
 
-  var hypotenuse = document.getElementById('hypotenuse').value;
-  var adjacent = document.getElementById('adjacent').value;
-  var opposite = document.getElementById('opposite').value;
+   var hypotenuse = document.getElementById('hypotenuse').value;
+   var adjacent = document.getElementById('adjacent').value;
+   var opposite = document.getElementById('opposite').value;
 
 
-  if (hypotenuse === opposite && opposite === hypotenuse && hypotenuse === adjacent) {
-    alert("It's an equilateral triangle!");
-  } else if (hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent) {
-    alert("It's an isosceles triangle!");
-  } else if ((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
-    alert("It's not a definite triangle ");
-  } else {
-    alert("It's a scalene triangle");
-  }
+   if (hypotenuse === opposite && opposite === hypotenuse && hypotenuse === adjacent) {
+     alert("It's an equilateral triangle!");
+   } else if (hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent) {
+     alert("It's an isosceles triangle!");
+   } else if ((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
+     alert("It's not a definite triangle ");
+   } else {
+     alert("It's a scalene triangle");
+   }
 
-}
+ }
 
-function reset(){
-  var hypotenuse=document.getElementById('hypotenuse').value;
-  var adjacent=document.getElementById('adjacent').value;
-  var opposite=document.getElementById('opposite').value;
+ function reset() {
+   var hypotenuse = document.getElementById('hypotenuse').value;
+   var adjacent = document.getElementById('adjacent').value;
+   var opposite = document.getElementById('opposite').value;
 
-}
+ }
